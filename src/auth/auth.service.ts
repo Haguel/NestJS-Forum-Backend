adcd2636e02d6cde6e835565ce8fb2d0f201844b
GlebHal <hammanov.gleb@gmail.com>
2023-06-10 18:14:11 +0300

    Implement register and login functional

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from 'src/users/models/users.model';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
-    login() {
-        console.log('login');
+    constructor(
+        private readonly jwtService: JwtService,
+        @InjectModel(User) private userRepository: typeof User
+    ) { }
+
+    async login(loginUserDto: LoginUserDto) {
+        try {
+            const user = await this.userRepository.findOne({ where: { email: loginUserDto.email } })
+
+            if (!user) {
+                throw new HttpException('There is no users with such email', HttpStatus.NOT_FOUND);
+            }
+
+            const isPasswordMatch: boolean = await bcrypt.compare(loginUserDto.password, user.password);
+
+            if (!isPasswordMatch) {
+                throw new HttpException('Wrong email or password', HttpStatus.FORBIDDEN);
+            }
+
+            const userLoginData = { ...loginUserDto, email: user.email };
+
+            const token: string = this.jwtService.sign(userLoginData);
+            return token;
+        } catch (err) {
+            console.log(err);
+        }
     }
 
-    unlogin() {
-        console.log('unlogin')
-    }
+    async register(createUserDto: CreateUserDto) {
+        try {
+            const saltRounds: number = Number(process.env.SALT_ROUNDS);
+            const hash: string = await bcrypt.hash(createUserDto.password, saltRounds);
 
-    register() {
-        console.log('register')
+            const userRegisterData = { ...createUserDto, password: hash }
+
+            await this.userRepository.create(userRegisterData);
+
+            const token: string = this.jwtService.sign(userRegisterData);
+            return token;
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
