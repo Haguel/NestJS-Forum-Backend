c617eb9c27e7374ee4135ee24d68ee749ee4603a
GlebHal <hammanov.gleb@gmail.com>
2023-06-10 18:14:11 +0300

    Implement register and login functional

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,19 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
     @Post('login')
-    login() {
-        this.authService.login();
-    }
-
-    @Post('unlogin')
-    unlogin() {
-        this.authService.unlogin();
+    login(@Body() loginUserDto: LoginUserDto) {
+        return this.authService.login(loginUserDto);
     }
 
     @Post('register')
-    register() {
-        this.authService.register();
-
+    register(@Body() createUserDto: CreateUserDto) {
+        return this.authService.register(createUserDto);
     }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from 'src/users/models/users.model';
+import { UserRole } from 'src/roles/models/user-roles.model';
+import { Role } from 'src/roles/models/roles.model';
+import { Post } from 'src/posts/models/posts.model';
+import { Complaint } from 'src/posts/models/complaints.model';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        global: true,
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '24h' },
+      })
+    }),
+    SequelizeModule.forFeature([User, UserRole, Role, Post, Complaint]),
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule { }
