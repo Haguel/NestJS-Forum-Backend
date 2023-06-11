a5a4c57b4c0e514931d3b3101d31125bfdcb712c
GlebHal <hammanov.gleb@gmail.com>
2023-06-11 14:14:54 +0300

    Made posts work logic

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './models/users.model';
+import { Post } from 'src/posts/models/posts.model';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectModel(User) private userRepository: typeof User) { }
+
+    async getUser(id: number) {
+        return await this.userRepository.findOne({
+            where: { id },
+            include: [Post]
+        });
+    }
+}
